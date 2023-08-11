7eef77e535d73e08d98c4a27577f1d2258d22586
Dineshg <dghadge2002@gmail.com>
2023-08-11 20:12:15 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# Titanic Survival Prediction
+
+This repository contains Python code for analyzing the Titanic dataset and predicting passenger survival using logistic regression.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Prerequisites](#prerequisites)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Code Overview](#code-overview)
+- [Results](#results)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Introduction
+
+This project aims to predict passenger survival on the Titanic based on various features using logistic regression. The dataset (`train.csv`) contains information about passengers' attributes and whether they survived the disaster.
+
+## Prerequisites
+
+Before you begin, ensure you have the following installed:
+
+- Python (version 3.1)
+- Pandas
+- Matplotlib
+- Seaborn
+- NumPy
+- Scikit-Learn
+
+## Installation
+
+1. Clone the repository:
+
+   ```bash
+   git clone https://github.com/dineshghadge2002/Titanic_ClassificationRegration.git
+   cd titanic-survival-prediction
+   ```
+
+2. Install the required dependencies:
+
+   ```bash
+   pip install pandas matplotlib seaborn numpy scikit-learn
+   ```
+
+## Usage
+
+1. Place the `train.csv` file in the project directory.
+
+2. Run the Jupyter Notebook or Python script `titanic_analysis.ipynb` to perform data preprocessing, exploratory data analysis, and model training.
+
+3. Review the generated visualizations and model evaluation metrics.
+
+## Code Overview
+
+The code performs the following steps:
+
+- Loading and exploring the dataset.
+- Visualizing categorical variables against survival using Seaborn count plots.
+- Cleaning the data by dropping unnecessary columns.
+- Handling missing age values by calculating the mean for each passenger class.
+- Creating dummy variables for categorical features.
+- Splitting the dataset into training and testing sets.
+- Building a logistic regression model using Scikit-Learn.
+- Evaluating the model's performance using a confusion matrix.
+
+## Results
+
+The logistic regression model achieved an accuracy of 80% on the test set. The confusion matrix provides insights into the model's performance, showing true positives, true negatives, false positives, and false negatives.
+
+## Contributing
+
+Contributions are welcome! If you find any issues or have suggestions for improvement, please feel free to submit a pull request.
---
+++ TitanicSurvived.py
@@ -0,0 +1,113 @@
+import pandas as pd
+import matplotlib as plt
+import seaborn as sns
+import numpy as np
+
+dataset=pd.read_csv("train.csv")
+
+print(dataset.columns)
+
+#Column Selection/Field Selection
+#y=dataset[["Survived"]]
+#print(y.head())
+
+gender=dataset['Sex']
+sns.countplot(data=dataset,x='Sex')
+sns.countplot(data=dataset,x='Sex',hue='Survived')
+
+sns.countplot(data=dataset,x='Pclass')
+sns.countplot(data=dataset,x='Pclass',hue='Survived')
+
+sns.countplot(data=dataset,x='SibSp')
+sns.countplot(data=dataset,x='SibSp',hue='Survived')
+
+sns.countplot(data=dataset,x='Parch')
+sns.countplot(data=dataset,x='Parch',hue='Survived')
+
+sns.countplot(data=dataset,x='Embarked')
+sns.countplot(data=dataset,x='Embarked',hue='Survived')
+
+dataset.drop("PassengerId",axis=1,inplace=True) #drop passengerid
+dataset.drop("Name",axis=1,inplace=True) #drop Name
+dataset.drop("Ticket",axis=1,inplace=True) #drop Ticket
+dataset.drop("Fare",axis=1,inplace=True) #drop Fare
+dataset.drop("Parch",axis=1,inplace=True) #drop parch
+dataset.drop("Cabin",axis=1,inplace=True) #drop Cabin
+
+#x=dataset[['Pclass','Sex','Age','SibSp','Cabin','Embarked']]
+
+#Working with Null values
+print(dataset.isnull())
+sns.heatmap(dataset.isnull())
+sns.heatmap(dataset.isnull() ,yticklabels=False , cmap="YlGnBu")
+print(dataset[dataset["Age"].isnull()]) #print null value
+
+#here we find out the mean value using the perticular pclass
+for i in range(1,4):
+    age=int(dataset[dataset["Pclass"]==i]['Age'].dropna().mean())
+    print(age)
+
+#fill the Null value
+def set_age(row):
+    Pclass=row[0]
+    age=row[1]
+    if np.isnan(age):
+        if Pclass==1:
+            return 38
+        elif Pclass==2:
+            return 29
+        else:
+            return 25
+    else:
+        return age
+    
+
+dataset[['Pclass','Age']].apply(set_age,axis=1)
+
+# Working on Catagorical variable
+#creating dummy variable
+Pclass=pd.get_dummies(dataset['Pclass'],drop_first=True)
+Pclass.head()
+
+Sex=pd.get_dummies(dataset['Sex'],drop_first=True)
+Sex.head()
+
+SibSp=pd.get_dummies(dataset['SibSp'],drop_first=True)
+SibSp.head()
+
+Embarked=pd.get_dummies(dataset['Embarked'],drop_first=True)
+Embarked.head()
+
+#we have to drop catagorical variable
+dataset.drop("Pclass",axis=1,inplace=True)
+dataset.drop("Sex",axis=1,inplace=True)
+dataset.drop("SibSp",axis=1,inplace=True)
+dataset.drop("Embarked",axis=1,inplace=True)
+
+y=dataset[['Survived']]
+dataset.drop("Survived",axis=1,inplace=True)
+
+#use that variable that we have converted into dummy variable 
+dataset=pd.concat([Pclass,Sex,SibSp,Embarked] , axis=1)
+print(dataset.head())
+
+#in some version of python feature name should be string
+x.columns=x.columns.astype(str) 
+x.columns
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+x_train,x_test,y_train,y_test=train_test_split( x, y, test_size=0.20, random_state=42)
+
+print(x_train)
+print(y_train)
+model=LogisticRegression()
+model.fit(x_train,y_train)
+print(model.coef_)
+print(model.intercept_)
+y_pred=model.predict(x_test)
+print(y_pred)
+
+#confusion matrix
+from sklearn.metrics import confusion_matrix
+print(confusion_matrix(y_test,y_pred))
